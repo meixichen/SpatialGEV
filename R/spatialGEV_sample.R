ee75fb3b07deab282846595e5949a833fb07e105
meixichen <meixichen2010@gmail.com>
2022-03-03 23:39:26 -0500

    Add feature for model abs in fit and sample functions

--- R/spatialGEV_sample.R
+++ R/spatialGEV_sample.R
@@ -3,6 +3,7 @@
 #' @param model A fitted spatial GEV model object of class `spatialGEVfit`
 #' @param n_draw Number of draws from the posterior distribution
 #' @param observation whether to draw from the posterior distribution of the GEV observation?
+#' @param loc_ind A vector of location indices to sample from. Default is all locations.
 #' @return An object of class `spatialGEVsam`, which is a list of matrices containing the 
 #' joint posterior draws of the parameters and optionally the GEV observations.
 #' @examples 
@@ -28,12 +29,13 @@
 #'                       reparam_s = "positive",
 #'                       kernel = "matern",
 #'                       silent = TRUE) 
-#' sam <- spatialGEV_sample(model = fit, n_draw = 5000, observation = TRUE)
+#' sam <- spatialGEV_sample(model = fit, n_draw = 100, 
+#'                          observation = TRUE, loc_ind=1:10)
 #' print(sam)
 #' summary(sam)
 #' }
 #' @export
-spatialGEV_sample <- function(model, n_draw, observation=FALSE){
+spatialGEV_sample <- function(model, n_draw, observation=FALSE, loc_ind=NULL){
   # Extract info from model
   kernel <- model$kernel
   rep <- model$report
@@ -47,6 +49,9 @@ spatialGEV_sample <- function(model, n_draw, observation=FALSE){
   }
   else if (length(random) == 2){
     mod <- "ab"
+  }
+  else if (length(random) == 3){
+    mod <- "abs"
   }
   else {
     stop("Cannot identify which GEV parameters are random.")
@@ -69,8 +74,16 @@ spatialGEV_sample <- function(model, n_draw, observation=FALSE){
       ind2rm <- setdiff(1:length(random_ind), meshidxloc) 
       joint_cov <- joint_cov[-ind2rm, -ind2rm]
     }
-    else {
+    else if (mod == "ab") {
       ind2rm <- setdiff(1:length(random_ind), c(meshidxloc, length(random_ind)/2 + meshidxloc))
+      mean_random <- mean_random[-ind2rm]
+      joint_cov <- joint_cov[-ind2rm, -ind2rm] 
+    }
+    else {# if mod="abs"
+      ind2rm <- setdiff(1:length(random_ind), 
+			c(meshidxloc, # indices of a in the random effects vec
+			  length(random_ind)/3 + meshidxloc, # indices of log_b in the random vec
+			  length(random_ind)/3*2 + meshidxloc)) # indices of s in  the random vec
       mean_random <- mean_random[-ind2rm]
       joint_cov <- joint_cov[-ind2rm, -ind2rm] 
     }
@@ -79,7 +92,21 @@ spatialGEV_sample <- function(model, n_draw, observation=FALSE){
   par_names_fixed <- names(mean_fixed) # extract parameter names for the fixed effects
   joint_mean <- c(mean_random, mean_fixed)
   names(joint_mean) <- c(par_names_random, par_names_fixed) # modify parameter names
-  joint_post_draw <- mvtnorm::rmvnorm(n_draw, joint_mean, joint_cov) # sample from the joint MVN
+  fixed_ind <- (length(mean_random)+1):length(joint_mean) # indices of the fixed effects
+
+  # Determine the positions of the samples based on location indices to be sampled
+  if (is.null(loc_ind)){
+    loc_ind <- 1:n_loc
+    sam_ind <- 1:length(joint_mean)
+  }
+  else{
+    loc_ind <- sort(loc_ind)
+    patterns <- c(paste0("a", loc_ind), paste0("log_b", loc_ind), paste0("s", loc_ind))
+    sam_ind <- which(names(joint_mean) %in% patterns)
+    sam_ind <- c(sam_ind, fixed_ind) 
+  }
+  # Sample from joint normal
+  joint_post_draw <- mvtnorm::rmvnorm(n_draw, joint_mean[sam_ind], joint_cov[sam_ind, sam_ind])
   output_list <- list(parameter_draws=joint_post_draw)
   # run below only if posterior draws of GEV observations are wanted
   if (observation){
@@ -99,23 +126,27 @@ spatialGEV_sample <- function(model, n_draw, observation=FALSE){
       }
     }
     y_draws <- rep(NULL, n_draw) # <<==== TODO: Should allocate space for y_draws. Tried to preallocate a large matrix but got error message about not enough memory.
-    for (i in 1:n_loc){
+    for (i in loc_ind){
       loc_draw <- rep(NA, n_draw)
       for (j in 1:n_draw){
-        a_draw <- joint_post_draw[j, i]
+        a_draw <- joint_post_draw[j, paste0("a", i)]
         if (mod == "a"){
-          b_draw <- joint_post_draw[j, n_loc+1]
-          s_draw <- s_draw_fun(j, n_loc+2)
+          b_draw <- joint_post_draw[j, "log_b"]
+          s_draw <- s_draw_fun(j, "s")
         }
-        else {
-          b_draw <- joint_post_draw[j, i+n_loc]
-          s_draw <- s_draw_fun(j, 2*n_loc+1)
+        else if (mod == "ab"){
+          b_draw <- joint_post_draw[j, paste0("log_b", i)]
+          s_draw <- s_draw_fun(j, "s")
         }
+	else { # if mod == "abs"
+          b_draw <- joint_post_draw[j, paste0("log_b", i)]
+          s_draw <- s_draw_fun(j, paste0("s", i))
+	}
         loc_draw[j] <- rgev(1, loc = a_draw, scale = exp(b_draw), shape = s_draw)
       }
       y_draws <- cbind(y_draws, loc_draw)
     }
-    colnames(y_draws) <- paste0("y", 1:n_loc)
+    colnames(y_draws) <- paste0("y", loc_ind)
     output_list[["y_draws"]] <- y_draws
   }
   class(output_list) <- "spatialGEVsam"
