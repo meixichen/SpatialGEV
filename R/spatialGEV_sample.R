0a29ee4517fbf60ea754a963aa15298086affe17
mlysy <mlysy@users.noreply.github.com>
2024-02-28 13:10:20 -0600

    moved examples to a separate folder

--- R/spatialGEV_sample.R
+++ R/spatialGEV_sample.R
@@ -6,34 +6,7 @@
 #' @param loc_ind A vector of location indices to sample from. Default is all locations.
 #' @return An object of class `spatialGEVsam`, which is a list of matrices containing the
 #' joint posterior draws of the parameters and optionally the GEV observations.
-#' @examples
-#' \donttest{
-#' library(SpatialGEV)
-#' n_loc <- 20
-#' a <- simulatedData$a[1:n_loc]
-#' logb <- simulatedData$logb[1:n_loc]
-#' logs <- simulatedData$logs[1:n_loc]
-#' y <- simulatedData$y[1:n_loc]
-#' locs <- simulatedData$locs[1:n_loc,]
-#' beta_a <- mean(a); beta_b <- mean(logb)
-#' fit <- spatialGEV_fit(y, locs = locs, random = "ab",
-#'                       init_param = list(beta_a = beta_a,
-#'                                         beta_b = beta_b,
-#'                                         a = rep(0, n_loc),
-#'                                         log_b = rep(0, n_loc),
-#'                                         s = 0,
-#'                                         log_sigma_a = 0,
-#'                                         log_kappa_a = 0,
-#'                                         log_sigma_b = 0,
-#'                                         log_kappa_b = 0),
-#'                       reparam_s = "positive",
-#'                       kernel = "matern",
-#'                       silent = TRUE)
-#' sam <- spatialGEV_sample(model = fit, n_draw = 100,
-#'                          observation = TRUE, loc_ind=1:10)
-#' print(sam)
-#' summary(sam)
-#' }
+#' @example examples/spatialGEV_sample.R
 #' @export
 spatialGEV_sample <- function(model, n_draw, observation=FALSE, loc_ind=NULL) {
   # Extract info from model
